e0d3085ec22934622d34baa707b3ff0448c4c4c3
marcosfabio <62723316+marcosfabio@users.noreply.github.com>
2021-06-29 11:22:42 -0300

    add e remove
    Adicionadas funcionalidades de adicionar e remover as transações.

--- script.js
+++ script.js
@@ -87,12 +87,13 @@ const DOM = {
 
   addTransaction(transaction, index) {
     const tr = document.createElement('tr');
-    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+    tr.dataset.index = index;
 
     DOM.transactionsContainer.appendChild(tr);
   },
 
-  innerHTMLTransaction(transaction) {
+  innerHTMLTransaction(transaction, index) {
     const cssClass = transaction.amount > 0 ? 'income' : 'expense';
 
     const amount = Utils.formatCurrency(transaction.amount);
@@ -105,7 +106,7 @@ const DOM = {
         <td class="date">${transaction.date}</td>
 
         <td>
-            <img src="./assets/minus.svg" alt="Remover transação" onClick="Transactions.remove" />
+            <img src="./assets/minus.svg" alt="Remover transação" onclick="Transactions.remove(${index})" />
         </td>
       `;
 
@@ -143,19 +144,95 @@ const Utils = {
 
     return signal + value;
   },
+
+  formatAmount(value) {
+    value = Number(value) * 100;
+    console.log(value);
+    return value;
+  },
+
+  formatDate(date) {
+    const splittedDate = date.split('-');
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+  },
 };
 
 const Form = {
+  description: document.querySelector('input#description'),
+  amount: document.querySelector('input#amount'),
+  date: document.querySelector('input#date'),
+
+  getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value,
+    };
+  },
+
+  validateFields() {
+    const { description, amount, date } = Form.getValues();
+    if (
+      description.trim() === '' ||
+      amount.trim() === '' ||
+      date.trim() === ''
+    ) {
+      throw new Error('Por favor preencha todos os campos');
+    }
+  },
+
+  formatValues() {
+    let { description, amount, date } = Form.getValues();
+    amount = Utils.formatAmount(amount);
+    date = Utils.formatDate(date);
+
+    return {
+      description,
+      amount,
+      date,
+    };
+  },
+
+  saveTransaction(transaction) {
+    Transactions.add(transaction);
+  },
+
+  clearFields() {
+    Form.description.value = '';
+    Form.amount.value = '';
+    Form.date.value = '';
+  },
+
   submit(event) {
     event.preventDefault();
+
+    try {
+      Form.validateFields();
+      const transaction = Form.formatValues();
+      Form.saveTransaction(transaction);
+      Form.clearFields();
+      Modal.close();
+    } catch (error) {
+      alert(error.message);
+    }
   },
 };
 
 const App = {
   init() {
-    Transactions.all.forEach((transaction) => {
-      DOM.addTransaction(transaction);
+    Transactions.all.forEach((transaction, index) => {
+      DOM.addTransaction(transaction, index);
     });
+
+    /*
+    
+    poderia ser escrito da seguinte maneira:
+
+    Transactions.all.forEach(DOM.addTransaction);
+
+    porque os parâmetros passados no forEach são os mesmos passados na função addTransaction.
+    
+    */
 
     DOM.updateBalance();
   },
@@ -168,16 +245,3 @@ const App = {
 // Inicialização da Aplicação.
 
 App.init();
-
-/*
-
-Transactions.add({
-  id: 5,
-  description: 'Pagamento 2',
-  amount: 36521,
-  date: '27/06/2021',
-});
-
-Transactions.remove(4);
-
-*/
